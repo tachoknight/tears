3a19bee3b4c8063ad9e799a269ea0b62fc44e259
Anders Johansson <anders@truenull.se>
2014-04-21 21:53:42 +0200

    Fixed path of sodium install script

--- scripts/install-sodium.sh
+++ scripts/install-sodium.sh
@@ -2,5 +2,5 @@
 # Installs sodium on travis
 set -ex
 wget https://github.com/jedisct1/libsodium/archive/master.zip -O /tmp/sodium.tar.gz
-tar -xzvf sodium.tar.gz
+tar -xzvf /tmp/sodium.tar.gz
 cd sodium-master && ./configure --prefix=/usr && make && make check && make install
