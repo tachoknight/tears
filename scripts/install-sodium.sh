67ba3b0bc0cf1fef50dcf1551a2685c036bebadf
Anders Johansson <anders@truenull.se>
2014-04-21 22:55:22 +0200

    Forgot sudo in sodium install

--- scripts/install-sodium.sh
+++ scripts/install-sodium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs sodium on travis
 set -ex
-wget https://github.com/jedisct1/libsodium/archive/master.tar.gz -O /tmp/sodium.tar.gz
-tar -xzf /tmp/sodium.tar.gz
-cd libsodium-master && ./autogen.sh && ./configure --prefix=/usr && make && make check && make install
+wget -q https://github.com/jedisct1/libsodium/archive/master.tar.gz -O /tmp/sodium.tar.gz
+ar -xzf /tmp/sodium.tar.gz
+cd libsodium-master && ./autogen.sh && ./configure -prefix=/usr && make -s && make check && sudo make install
