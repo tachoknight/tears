3db06341b88b7a304cff71f67b97866c11a1a564
Anders Johansson <anders@truenull.se>
2014-04-21 23:00:11 +0200

    Accidentally deleted t in tar...

--- scripts/install-sodium.sh
+++ scripts/install-sodium.sh
@@ -2,5 +2,5 @@
 # Installs sodium on travis
 set -ex
 wget -q https://github.com/jedisct1/libsodium/archive/master.tar.gz -O /tmp/sodium.tar.gz
-ar -xzf /tmp/sodium.tar.gz
+tar -xzf /tmp/sodium.tar.gz
 cd libsodium-master && ./autogen.sh && ./configure -prefix=/usr && make -s && make check && sudo make install
