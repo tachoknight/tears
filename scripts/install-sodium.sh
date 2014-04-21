869e02a2e3bdfee55ce4c73b68425db732994cba
Anders Johansson <anders@truenull.se>
2014-04-21 22:03:32 +0200

    Fixed wrong filetype being downloaded from GH

--- scripts/install-sodium.sh
+++ scripts/install-sodium.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Installs sodium on travis
 set -ex
-wget https://github.com/jedisct1/libsodium/archive/master.zip -O /tmp/sodium.tar.gz
+wget https://github.com/jedisct1/libsodium/archive/master.tar.gz -O /tmp/sodium.tar.gz
 tar -xzvf /tmp/sodium.tar.gz
-cd sodium-master && ./configure --prefix=/usr && make && make check && make install
+cd libsodium-master && ./configure --prefix=/usr && make && make check && make install
