dce030926b2160fc9773eab658b5a6a6b1c14bfa
Anders Johansson <anders@truenull.se>
2014-04-21 22:23:11 +0200

    Installing sodium build dependencies on Travis.

--- scripts/install-sodium.sh
+++ scripts/install-sodium.sh
@@ -2,5 +2,5 @@
 # Installs sodium on travis
 set -ex
 wget https://github.com/jedisct1/libsodium/archive/master.tar.gz -O /tmp/sodium.tar.gz
-tar -xzvf /tmp/sodium.tar.gz
-cd libsodium-master && ./configure --prefix=/usr && make && make check && make install
+tar -xzf /tmp/sodium.tar.gz
+cd libsodium-master && ./autogen.sh && ./configure --prefix=/usr && make && make check && make install
